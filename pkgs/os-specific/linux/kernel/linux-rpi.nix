3f9c68b0eae7638f166dc7a99ea831a825581a65
Pete <pete3n@protonmail.com>
2023-12-10 05:08:11 -0500

    5 option test

--- pkgs/os-specific/linux/kernel/linux-rpi.nix
+++ pkgs/os-specific/linux/kernel/linux-rpi.nix
@@ -21,7 +21,7 @@ lib.overrideDerivation (buildLinux (args // {
     "2" = "bcm2709_defconfig";
     "3" = if stdenv.hostPlatform.isAarch64 then "bcmrpi3_defconfig" else "bcm2709_defconfig";
     "4" = "bcm2711_defconfig";
-    "Z2W" = "bcm2711_defconfig";
+    "5" = "bcm2711_defconfig";
   }.${toString rpiVersion};
 
   features = {
